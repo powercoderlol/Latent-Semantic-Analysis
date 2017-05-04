3313f8b93bcea0cec7ee08be96132da740d96fa8
Ivan <ipolyiakov@gmail.com>
2017-05-05 02:03:11 +0300

    update Constants for Menu, add version

--- src/main/java/com/controller/utils/Constants.java
+++ src/main/java/com/controller/utils/Constants.java
@@ -5,6 +5,18 @@ package com.controller.utils;
  */
 public class Constants {
 
+    public static String version = "0.1";
+
     public static String delimiters = " !,?;:_()[]$-@~/*#&\".%»«";
 
+    public static final String MAIN_MENU_NAME = "LSA TOOLS MAIN MENU";
+    public static final String EMJL_PROCESS_MENU_NAME = "EMJL LSA TOOLS MENU";
+    public static final String FILES_JOB_MENU_NAME = "WORKING WITH FILES MENU";
+
+    public static final String OUTPUT_DIRECTORY = "outputFiles/";
+    public static final String SOURCE_DIRECTORY = "sourceFiles/";
+    public static final String OUTPUT_DIR = "outputFiles";
+
+    public static final String WELCOME_MSG = "Welcome to Latent Semantic Analysis Tools, version " + version;
+    public static final String SUCCESS_CREATING_FOLDER = "New foder was created successfully.\n";
 }
