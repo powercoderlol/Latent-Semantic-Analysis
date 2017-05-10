5714c6f4bc8361f1a21e3b3dc52fd5304c422a34
Ivan <ipolyiakov@gmail.com>
2017-05-10 19:31:50 +0300

    add svd for all to all paragraphs mode and new submenu

--- src/main/java/com/controller/utils/MenuExecutor.java
+++ src/main/java/com/controller/utils/MenuExecutor.java
@@ -54,6 +54,7 @@ public class MenuExecutor {
 
         subMenuEMJLLSATools.putCommand("Set file to process", () -> setFileToProcess());
         subMenuEMJLLSATools.putCommand("Get SVD result for paragraph in series", () -> getSvdInSeries());
+        subMenuEMJLLSATools.putCommand("Get SVD result for paragraph all to all", () -> getSvdAllToAll());
         subMenuEMJLLSATools.putCommand("Print V^t Matrix on screen", () -> printVtMatrix());
         subMenuEMJLLSATools.putCommand("Return to LSA tools menu", () -> activateMenu(subMenuEMJLLSA));
         subMenuEMJLLSATools.putCommand("Return to Main Menu", () -> activateMenu(currentMenu));
@@ -75,20 +76,47 @@ public class MenuExecutor {
 
     private void getSvdInSeries() {
         String outputFileName, buffFileName, buffDate;
-        Date currentDate = new Date();
+        Date currentDate;
         if (filename == null) {
             System.out.println("Set file to process first");
         } else {
             if (TextReader.checkCreateShowFolder(OUTPUT_DIRECTORY, false, "exist")) {
                 SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy'_T_'HH-mm-ss");
+                currentDate = new Date();
                 buffDate = sdf.format(currentDate);
                 buffFileName = filename.substring(SOURCE_DIRECTORY.length(),filename.length()-4);
                 outputFileName = OUTPUT_DIRECTORY + "SVD comparsion in series " + buffFileName + " " + buffDate + ".txt";
                 DataPrinter.printComparsionVMatrix(lsaAlgoMachine.getComparsion(), lsaAlgoMachine.getCols());
                 DataPrinter.printDataInFile(outputFileName, lsaAlgoMachine.getComparsion());
-
             }
         }
+    }
+
+    private void getSvdAllToAll() {
+        String outputDirName, buffDate, buffFileName;
+        Date currentDate;
+        SimpleDateFormat sdf;
+        DataController dc;
+        if (filename == null) {
+            System.out.println("Set file to process first");
+        } else {
+            if (TextReader.checkCreateShowFolder(OUTPUT_DIRECTORY, false, "exist")) {
+                System.out.println("Calculation in progress...");
+                sdf = new SimpleDateFormat("dd-MM-yyyy_'T'_HH-mm-ss");
+                currentDate = new Date();
+                buffDate = sdf.format(currentDate);
+                buffFileName = filename.substring(SOURCE_DIRECTORY.length(), filename.length()-4);
+                outputDirName = OUTPUT_DIRECTORY + "SVD comparsion all to all " + buffFileName + " " + buffDate + "/";
+                if(TextReader.checkCreateShowFolder(outputDirName, false, "exist")) {
+                    for(int i = 0; i < lsaAlgoMachine.getCols(); i++) {
+                        buffFileName = outputDirName + (i+1) + " paragraph.txt";
+                        DataPrinter.printDataInFile(buffFileName, lsaAlgoMachine.getComparsionAll(i));
+                    }
+                }
+                System.out.println("Done! See files with data in " + outputDirName + " directiry");
+            }
+        }
+
     }
 
     private void printVtMatrix() {
